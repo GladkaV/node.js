105103e7d6b0498169cb6a322ce50fd59cc3ba32
GladkaV <vktr.gldk10.98@gmail.com>
2021-10-08 01:25:06 +0300

    added mongoose

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
-
-const userRouter = require('./router/user.router');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/test');
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+const userRouter = require('./router/user.router');
 
 app.use('/users', userRouter);
 
